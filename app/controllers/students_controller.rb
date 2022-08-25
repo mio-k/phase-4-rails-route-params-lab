4c3a11fe6149936e7eecd5a703eabbb66a88ff0d
mio kataoka <mio.kanna@gmail.com>
2022-08-24 20:13:02 -0700

    commit tree 752237d0d336c953857dd0d383febb609ea1ab38

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.filter do |student|
+      if params[:name] = student.name
+      end
     render json: students
+  end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
   end
 
 end
